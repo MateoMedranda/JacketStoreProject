85a43a6563061c0a41480ee0d97f2b06b48d7f3c
Mateo Medranda <matemedranda15@gmail.com>
2025-02-14 16:29:02 -0500

    1.18.0

--- 4_CODE/php/registraProducto.php
+++ 4_CODE/php/registraProducto.php
@@ -1,22 +1,30 @@
 <?php
-$descripcion = !empty($_POST['nombre'])? $_POST['nombre']:'';
-$material = !empty($_POST['apellido'])? $_POST['apellido']:'';
-$color = !empty($_POST['cedula'])? $_POST['cedula']:'';
-$stock = !empty($_POST['correo'])? $_POST['correo']:'';
-$talla = !empty($_POST['correo'])? $_POST['correo']:'';
-$estado = !empty($_POST['correo'])? $_POST['correo']:'';
-$precio = !empty($_POST['correo'])? $_POST['correo']:'';
-$descuento = !empty($_POST['correo'])? $_POST['correo']:'';
+$descripcion = !empty($_POST['descripcion'])? $_POST['descripcion']:'';
+$material = !empty($_POST['material'])? $_POST['material']:'';
+$color = !empty($_POST['color'])? $_POST['color']:'';
 
-if($nombre&&$apellido&&$cedula&&$email){
+if (isset($_POST['opciones']) && isset($_POST['cantidades'])) {
+    $tallas = $_POST['opciones'];  
+    $cantidades = $_POST['cantidades']; 
     include("conexion.php");
-    $consulta = <<<FIN
-    insert into usuarios(Nombre,Apellido,Cedula,Email)
-    values('$nombre','$apellido','$cedula','$email')
-    FIN;
-    if(!mysqli_query($conexion,$consulta)){
-        die('No se pudo agregar el registro');
+
+    for ($i = 0; $i < count($tallas); $i++) {
+
+        echo "<h3>Datos Recibidos:</h3>";
+    echo "<strong>Descripción:</strong> " . htmlspecialchars($descripcion) . "<br>";
+    echo "<strong>Material:</strong> " . htmlspecialchars($material) . "<br>";
+    echo "<strong>Color:</strong> " . htmlspecialchars($color) . "<br>";
+    echo "<h3>Detalles de productos:</h3>";
+        
+        $registro = <<<FIN
+        insert into producto(PRODUCTO_DESCRIPCION,PRODUCTO_MATERIAL,PRODUCTO_COLOR,PRODUCTO_STOCK,PRODUCTO_TALLA,PRODUCTO_ESTADO,PRODUCTO_PRECIO,PRODUCTO_DESCUENTO)
+        values('$descripcion','$material','$color','$cantidades[$i]','$tallas[$i]','pendiente','0','0')
+        FIN;
+        if (!mysqli_query($conexion, $registro)) {
+            die('❌ No se pudo agregar el registro: ' . mysqli_error($conexion));
+        }
     }
 }
-header('Location:../index.php');
+
+header('Location:../NicePage/Empresa/inventario.html');
 ?>
